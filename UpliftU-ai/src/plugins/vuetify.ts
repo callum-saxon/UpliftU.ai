61705610b8a6b6fbf9af68b673e66a318b73c7d1
Callum Saxon <78568087+callum-saxon@users.noreply.github.com>
2023-11-09 12:36:09 +0000

    v-Theme Setup
    Dark and Light mode dependant on time of day

--- UpliftU-ai/src/plugins/vuetify.ts
+++ UpliftU-ai/src/plugins/vuetify.ts
@@ -4,33 +4,56 @@
  * Framework documentation: https://vuetifyjs.com`
  */
 
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/styles'
-import { createApp } from 'vue'
-import { createVuetify, ThemeDefinition } from 'vuetify'
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
+import { createApp } from "vue";
+import { createVuetify, ThemeDefinition } from "vuetify";
+import moment from "moment";
+
+const isDaytime = () => {
+  const now = moment();
+  const sunrise = moment().set({ hour: 6, minute: 0, second: 0 });
+  const sunset = moment().set({ hour: 18, minute: 0, second: 0 });
+
+  return now.isBetween(sunrise, sunset);
+};
 
 const myCustomLightTheme = {
   dark: false,
   colors: {
-    background: '#FFFFFF',
-    color: '#121212',
-    surface: '#FFFFFF',
-    primary: '#6200EE',
-    'primary-darken-1': '#3700B3',
-    secondary: '#03DAC6',
-    'secondary-darken-1': '#018786',
-    error: '#B00020',
-    info: '#2196F3',
-    success: '#4CAF50',
-    warning: '#FB8C00',
+    background: "#FFFFFF",
+    primary: "#6200EE",
+    "primary-darken-1": "#3700B3",
+    secondary: "#03DAC6",
+    "secondary-darken-1": "#018786",
+    error: "#B00020",
+    info: "#2196F3",
+    success: "#4CAF50",
+    warning: "#FB8C00",
   },
-}
+};
+
+const myCustomDarkTheme = {
+  dark: true,
+  colors: {
+    background: "#121212",
+    primary: "#BB86FC",
+    "primary-darken-1": "#3700B3",
+    secondary: "#03DAC6",
+    "secondary-darken-1": "#018786",
+    error: "#CF6679",
+    info: "#2196F3",
+    success: "#4CAF50",
+    warning: "#FFC107",
+  },
+};
 
 export default createVuetify({
   theme: {
-    defaultTheme: 'myCustomLightTheme',
+    defaultTheme: isDaytime() ? "myCustomLightTheme" : "myCustomDarkTheme",
     themes: {
       myCustomLightTheme,
+      myCustomDarkTheme,
     },
   },
-})
+});
